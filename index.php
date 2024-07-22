0e71566b8eaf0542fde2cd5836810b8f8cacca1c
Shashwat <shashwatraj91@gmail.com>
2024-07-23 00:00:23 +0530

    connect to data base

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+  $username="root"
+  $servername="localhost"
+  $password=""
+  $database="notes"
+  $conn=mysqli_connect($servername,$username,$password,$database);
+  if(!$conn){
+    die("sorry we failed to connect: "+mysqli_connect_error());
+  }
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
