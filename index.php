423977a6f78d0347d3737c0e7ae03d44bc58575a
Shashwat <shashwatraj91@gmail.com>
2024-07-23 13:12:06 +0530

    DataTables added

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="//cdn.datatables.net/2.1.0/css/dataTables.dataTables.min.css">
   </head>
   <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -76,9 +78,9 @@
     <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
   </div>";
   }
-  else{
-    echo "notes cant added";
-  }
+  // else{
+  //   echo "notes cant added";
+  // }
 ?>
 </div>
 
@@ -102,8 +104,8 @@
               <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
     </div>
-    <div class="container">
-    <table class="table">
+    <div class="container my-4" >
+    <table class="table" id="myTable">
   <thead>
     <tr>
       <th scope="col">Sno</th>
@@ -131,5 +133,13 @@
       
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/2.1.0/js/dataTables.min.js"></script>
+    
+    <script>
+      $(document).ready(function(){
+        $('#myTable').DataTable();
+      });
+    </script>
   </body>
 </html>
