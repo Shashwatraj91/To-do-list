f9603559187a65815ccfdde3e60904faeda066d1
Shashwat <shashwatraj91@gmail.com>
2024-07-27 00:06:29 +0530

    update the note

--- index.php
+++ index.php
@@ -9,16 +9,27 @@
     die("sorry we failed to connect: "+mysqli_connect_error());
   }
   if($_SERVER['REQUEST_METHOD']=='POST'){
-    $title=$_POST['title'];
-    $desc=$_POST['desc'];
-    $sql="INSERT INTO `notes` (`title`, `desc`, `time`) VALUES ('$title', '$desc', current_timestamp())";
-    $result=mysqli_query($conn,$sql);
-    if($result){
-      $insert=true;
+    if(isset($_POST['snoEdit'])){
+      //update the record
+      $sno=$_POST['snoEdit'];
+      $title=$_POST['titleEdit'];
+      $desc=$_POST['descEdit'];
+      $sql="UPDATE `notes` SET `title` = '$title' , `desc` = '$desc' WHERE `notes`.`sno` = $sno ";
+      $result=mysqli_query($conn,$sql);
     }
     else{
-      echo "error";
+      $title=$_POST['title'];
+      $desc=$_POST['desc'];
+      $sql="INSERT INTO `notes` (`title`, `desc`, `time`) VALUES ('$title', '$desc', current_timestamp())";
+      $result=mysqli_query($conn,$sql);
+      if($result){
+        $insert=true;
+      }
+      else{
+        echo "error";
+      }
     }
+    
   }
 ?>
 
@@ -32,8 +43,90 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <link rel="stylesheet" href="//cdn.datatables.net/2.1.0/css/dataTables.dataTables.min.css">
+    
   </head>
   <body>
+    <!-- Button trigger modal -->
+<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+  Edit Modal
+</button>
+
+
+<!-- Modal -->
+<!-- <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="/TO-DO-LIST/index.php" method="post">
+              <div class="my-3">
+                  <h2>Add a Note</h2>
+                  <label for="title" class="form-label">Note Title</label>
+                  <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+                  
+              </div>
+              <div class="mb-3">
+                  <div class="mb-3">
+                      <label for="desc" class="form-label">Notes Description</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" name="desc" rows="3"></textarea>
+                  </div>
+              </div>
+              <!-- <div class="mb-3 form-check">
+              </div> -->
+              <!-- <button type="submit" class="btn btn-primary">Update Note</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>  -->
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+  Edit Modal
+</button>
+
+
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editModalLabel">Edit this Note</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="/TO-DO-LIST/index.php" method="post">
+                <div class="my-3">
+                    <h2>Add a Note</h2>
+                    <input type="hidden" name="snoEdit" id="snoEdit">
+                    <label for="title" class="form-label">Note Title</label>
+                    <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+                    
+                </div>
+                <div class="mb-3">
+                    <div class="mb-3">
+                        <label for="descEdit" class="form-label">Notes Description</label>
+                        <textarea class="form-control" id="descEdit" name="descEdit" rows="3"></textarea>
+                    </div>
+                </div>
+                <!-- <div class="mb-3 form-check">
+                </div> -->
+                <button type="submit" class="btn btn-primary">Update Note</button>
+          </form>
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">iNotes</a>
@@ -118,16 +211,17 @@
       <?php
         $sql="SELECT * FROM `notes`";
         $result = mysqli_query($conn,$sql);
+        $sno=0;
         while($row=mysqli_fetch_assoc($result)){
+          $sno=$sno+1;
           echo "<tr>
-          <th scope='row'>".$row["sno"]."</th>
+          <th scope='row'>".$sno."</th>
           <td>".$row["title"]."</td>
           <td>".$row["desc"]."</td>
-          <td>delet edit</td>
+          <td><button class='edit btn btn-sm btn-primary' id=".$row['sno'].">Edit</button> <button class='btn btn-sm btn-primary'>Delete</button></td>
         </tr>";
         } 
       ?>
-    
   </tbody>
 </table>
       
@@ -140,6 +234,22 @@
       $(document).ready(function(){
         $('#myTable').DataTable();
       });
+    </script>
+    <script>
+      edits=document.getElementsByClassName('edit');
+      Array.from(edits).forEach((element)=>{
+        element.addEventListener("click",(e)=>{
+          tr=e.target.parentNode.parentNode;
+          title=tr.getElementsByTagName('td')[0].innerText;
+          desc=tr.getElementsByTagName('td')[1].innerText;
+          titleEdit.value=title;
+          descEdit.value=desc;
+          snoEdit.value=e.target.id;
+          console.log(e.target.id);
+          // const myModalAlternative = new bootstrap.Modal('#editModal', options);
+          $('#editModal').modal('toggle');
+        })
+      })
     </script>
   </body>
 </html>
